d9fba5dcaea69556424d5f3c43878ca078d06b49
Javier Roman Minguez <javierroman@javierroman.com>
2023-02-02 17:51:44 +0100

    Create a pinia store for the team name, spots and members

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,15 +2,21 @@
     import TeamMembers from "../components/Teams/TeamMembers.vue"
     import TeamHeader from "../components/Teams/TeamHeader.vue"
     import TeamFooter from "../components/Teams/TeamFooter.vue"
-    import team from "../team.json"
+    //import team from "../team.json"
+    import { useTeamStore } from "../stores/TeamStore"
+    let team = useTeamStore();
+    team.fill()
+    setTimeout(()=>{
+        team.grow(3)
+    }, 3000)
 </script>
 
 <template>
     <div class="flex justify-between flex-col">
-    <TeamHeader :team="team" />
+    <TeamHeader />
     <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
-        <TeamMembers :team="team" />        
+        <TeamMembers />        
     </div>
-    <TeamFooter :team="team" />
+    <TeamFooter />
     </div>
 </template>
